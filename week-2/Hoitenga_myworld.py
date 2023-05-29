569524c7f870b2866887b2e0a4b92b77afdcf2a0
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-05-29 14:27:54 -0500

    adding comments

--- week-2/Hoitenga_myworld.py
+++ week-2/Hoitenga_myworld.py
@@ -1,1 +1,13 @@
-print("You are now in Hoitenga’s world!")
+#################################################
+# Title: Assignment 2.3 Python
+# Author: Professor Krasso
+# Date: May 29, 2023
+# Modified By: Jennifer Hoitenga
+# Description: Write the necessary to output the value “You are now in <yourLastName>’s world!”
+# Sources Used: 
+# Python Guide provided by Professor Krasso
+# Learn Python in One Hour by Mosh
+#################################################
+
+# Creating my first python program
+print("You are now in Hoitenga's world!")
